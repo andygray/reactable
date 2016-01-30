bd88a8a337d14af5c6ef2fe6aa66f2443ca84bb4
Andy Gray <andyg@cakesolutions.net>
2016-01-30 17:22:59 +0000

    adjusting table values

--- api/Table.js
+++ api/Table.js
@@ -5,6 +5,7 @@ var tableRoutes = function (app, Competition, Pick) {
     app.get('/table/:competitionId', function (req, res) {
         console.log('GET /table/' + req.params.competitionId);
 
+
         Competition
             .findOne({'_id': req.params.competitionId})
             .exec()
@@ -14,6 +15,19 @@ var tableRoutes = function (app, Competition, Pick) {
                         player: selection.selection,
                         score: selection.score
                     };
+                });
+
+                // adjust values randomly
+                tempTable = _.map(tempTable, function (row) {
+                    var x = Math.floor((Math.random() * 10) + 1);
+                    if (x < 3) {
+                        row.score = row.score + 1;
+                    }
+                    if (x > 3 && x < 7) {
+                        row.score = row.score - 1;
+                    }
+
+                    return row;
                 });
 
                 res.send(_.orderBy(tempTable, ['score'], ['asc']));
@@ -29,13 +43,13 @@ var tableRoutes = function (app, Competition, Pick) {
         console.log('PUT /table/push/' + req.params.competitionId + ' BODY: ' + JSON.stringify(req.body));
 
         Competition
-            .findByIdAndUpdate(
-            {'_id': req.params.competitionId, 'selections.selection': req.body.selectionId},
-            {
-                'updated': Number((new Date().getTime() / 1000).toFixed(0)),
-                '$push': {'selections.$.score': 33 }
-            },
-            {new: true})
+            .findOne(req.params.competitionId)
+            .update({'selections.selection': req.body.selectionId}, {
+                updated: Number((new Date().getTime() / 1000).toFixed(0)),
+                $set: {
+                    'selections.$.score': req.body.score
+                }
+            })
             .exec()
             .then(function (game) {
                 res.send(game);
