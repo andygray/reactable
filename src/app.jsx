71d15e5822ef2e31b1dab01fd89bf8df66513b07
fletch <ste.fletcher@gmail.com>
2016-01-30 09:17:03 +0000

    build headers from response object when not passed as a property

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,7 @@ var ReactTable = require('./js/reactable.jsx');
 ReactDOM.render(
     <ReactTable
         text="React Table"
-        columnHeadings={['Position', 'Player', 'Score']}
+        //columnHeadings={['Position', 'Player', 'Score']}
         source="/table/56a52965d4c622243710639e">
     </ReactTable>,
     document.getElementById('react-container')
