04d0bb483bab15c080c3597c8229089421f36487
Andy Gray <andyg@cakesolutions.net>
2016-03-16 13:36:20 +0000

    CORS for localhost:3000

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.use(bodyParser.json());
 var api_path = __dirname + '/api';
 var models_path = __dirname + '/models';
 
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
+app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use(express.static(__dirname + '/public'));
 
